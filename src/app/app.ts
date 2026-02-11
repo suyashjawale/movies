ebaa8b4db4370abcd5e426b8e971eeb50cf6b1d3
Suyash Jawale <suyashjawale245@gmail.com>
2026-02-11 19:59:47 +0530

    Code added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [],
+	templateUrl: './app.html',
+	styleUrl: './app.scss'
 })
 export class App {
-  openLink(link: string) {
-      window.open(link)
-  }
+
+	links = signal<any>([]);
+
+	constructor(private http:HttpClient){}
+
+	ngOnInit() {
+		const headers = new HttpHeaders({
+			'Content-Type': 'application/json',
+			'X-Site-Identity': 'portfolio-admin-v1'
+		});
+
+		this.http.get('https://dashing-llama-639318.netlify.app/.netlify/functions/getMovies', { headers }).subscribe({
+			next: (data: any) => {
+				this.links.set(data)
+			},
+			error: err => {
+				this.links.set([]);
+			}
+		});
+	}
+
+	openLink(link: string) {
+		window.open(link)
+	}
 }
